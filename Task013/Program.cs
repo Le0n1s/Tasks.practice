db36b9f0c5bda2f5944ac01ea1ba00639c73af38
Sergey <diamlion21@gmail.com>
2022-10-10 23:20:17 +0500

    add Task013

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,1 +1,23 @@
 ﻿Console.WriteLine("Введите целое число: ");
+int a = int.Parse(Console.ReadLine());
+a = Math.Abs(a);
+if (a >= 100)
+{
+    if (a > 999)
+    {
+        int i = 10;
+        while (a / i > 1000)
+        {
+            i = i * 10;
+        }
+        Console.WriteLine("Третья цыфра числа: " + (a / i) % 10);
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра числа: " + a % 10);
+    }
+}
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
