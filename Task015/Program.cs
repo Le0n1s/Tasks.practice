72c0f8c9131f6e0e9f005b203dec1d77dafde81f
Sergey <diamlion21@gmail.com>
2022-10-11 20:05:13 +0500

    add Task 015

---
+++ Task015/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber == 6 | dayNumber == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Ррработаем!");
+}
