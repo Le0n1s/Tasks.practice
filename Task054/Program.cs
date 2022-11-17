65b9f9d5b1412b30b67bdbc43e613771d1f74726
Sergey <diamlion21@gmail.com>
2022-11-17 15:16:39 +0500

    add Task_054

---
+++ Task054/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] array = new Int32[4, 5];
+
+randomFilling(array, 0, 10);
+Console.WriteLine("Not sort");
+arrayOutput(array);
+sortLine(array);
+Console.WriteLine("Sort");
+arrayOutput(array);
+
+void randomFilling(int[,] arr, int min, int max)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void sortLine(int[,] arr)
+{
+    for (var i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1); k++)
+            {
+
+                if (arr[i, j] > arr[i, k])
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void arrayOutput(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+        }
+
+        Console.WriteLine(" ");
+    }
+}
